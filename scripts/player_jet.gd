ef9ef8413c0a209588279732f06bff2466121376
Dcarreon <dcarreonr@gmail.com>
2024-07-13 15:30:25 -0500

    player stops turning when velocity becomes zero

--- scripts/player_jet.gd
+++ scripts/player_jet.gd
@@ -10,9 +10,10 @@ extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	# ROTATION
-	var angle := global_position.angle_to_point(get_global_mouse_position())
-	var last_global_rotation := global_rotation
-	global_rotation = lerp_angle(last_global_rotation, angle, rotation_speed)
+	if (velocity != Vector2.ZERO):
+		var angle := global_position.angle_to_point(get_global_mouse_position())
+		var last_global_rotation := global_rotation
+		global_rotation = lerp_angle(last_global_rotation, angle, rotation_speed)
 
 	# MOVEMENT
 	if Input.is_action_pressed("ui_accept"):
@@ -20,5 +21,19 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
 	elif (velocity != Vector2.ZERO):
 		velocity = lerp(velocity, Vector2.ZERO, 0.02)
+		velocity = _round_vec2_to_whole(velocity)
 
 	move_and_slide()
+
+func _round_vec2_to_whole(vector: Vector2) -> Vector2:
+	if vector.x < 0:
+		vector.x = ceil(velocity.x)
+	elif vector.x > 0:
+		vector.x = floor(velocity.x)
+
+	if vector.y < 0:
+		vector.y = ceil(velocity.y)
+	elif vector.y > 0:
+		vector.y = floor(velocity.y)
+
+	return vector
